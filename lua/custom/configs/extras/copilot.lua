3c46fce0b82644a8c2e514eb7dfc8b9da9952149
Tiberius Dourado <tiberius.dourado@tailwindsw.com>
2023-09-10 16:42:43 -0300

    Initial config with custom lazy.nvim plugins

---
+++ lua/custom/configs/extras/copilot.lua
@@ -0,0 +1,14 @@
+local spec = {
+  {
+    "zbirenbaum/copilot.lua",
+    event = { "InsertEnter" },
+    cmd = { "Copilot" },
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+      }
+    }
+  },
+}
+
+return spec
